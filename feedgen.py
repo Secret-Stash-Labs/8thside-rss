71072740c9e1051916ce379767e506d4e30f6017
zdevore3c <zdevore@gmail.com>
2024-01-29 13:15:28 -0500

    update feedgen

--- feedgen.py
+++ feedgen.py
@@ -7,12 +7,29 @@ import time
 import hashlib
 import chromedriver_autoinstaller
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
-chromedriver_autoinstaller.install()
 
-# Set up the WebDriver
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
+chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+
+chrome_options = Options()
+options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+]
+for option in options:
+    chrome_options.add_argument(option)
+
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
 
 # Create a new RSS feed
 feed = Rss201rev2Feed(
